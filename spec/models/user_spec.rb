f174970c039fbeca66df0aafcee6dde8af6b73d8
Anton <toshaok@mail.ru>
2013-12-11 10:59:07 +0400

    Chapter 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -80,6 +80,15 @@ describe User do
   		end
   	end
   end
+  describe "email address with mixed case" do
+  	let(:mixed_case_email) {"Foo@ExAmpLE.COm"}
+  	
+  	it "should ba saved as all lower-case" do
+  		@user.email=mixed_case_email
+  		@user.save
+  		expect(@user.reload.email).to eq mixed_case_email.downcase
+  	end
+  end
   describe "when email address is already taken" do
   	before do
   		user_with_same_email = @user.dup
